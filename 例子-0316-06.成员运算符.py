882b844c81f398e3ecaf94608570031b9f757df5
blackplusy <osimeno@163.com>
2020-03-16 14:32:24 +0800

    Add files via upload

---
+++ 例子-0316-06.成员运算符.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+name='hello world'
+#字符串
+for i in name:
+    print(i)
+
+#列表
+li=['kobe','james','rose']
+for i in li:
+    print(i)
+
+#判断
+if 'e' in name:
+    print('e is here!!')
+    
