afe85c743902e7b5c4a1e8ce4a67a42d5041b197
blackplusy <osimeno@163.com>
2020-03-16 15:22:51 +0800

    Add files via upload

---
+++ 例子-0316-08.双分支判断.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+name=input('请输入叫做贝塔的原因：')
+if name=='乔碧萝':
+    print('yes its me!')
+else:
+    print('飞机刷没了！！！')
---
+++ 例子-0316-09.多分支.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+name=input('请输入您喜欢的主播:')
+if name=='pdd':
+    print('太乙真人！！！！')
+elif name=='大司马':
+    print('正方形打野')
+elif name=='醉叮当':
+    print('尼玛干他！！！')
+else:
+    print('关注一波刷个666！')
